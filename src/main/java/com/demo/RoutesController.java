f1b8aacbe10aa291f1d63052b1054aed7651a36b
harisonde <harisonde@yahoo.co.in>
2018-05-24 22:54:34 -0500

    added index page

---
+++ src/main/java/com/demo/RoutesController.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class RoutesController {
+	
+	@GetMapping("/greeting")
+	public String getIndexPage(@RequestParam(name="name", required=false, defaultValue="hari") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+}
---
+++ src/main/java/com/demo/ServicesController.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/main")
+public class ServicesController {
+
+	@GetMapping(value="/welcome")
+	public ResponseEntity<String> homePage() {
+		System.out.println("Hello, how are you!!!1");
+		return new ResponseEntity<>("Hello!!", HttpStatus.OK);
+	}	
+}
